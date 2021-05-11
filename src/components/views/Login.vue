b0abcfcb60a85d45cfb63021203edc0401c468c8
ivan riboldi <ivanriboldi@Air-de-ivan.fibertel.com.ar>
2021-05-10 22:37:45 -0300

    se creo la vista del login

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -1,13 +1,68 @@
 <template>
-  <h1> LOGIN</h1>
+  <div class="container-login">
+    <h2 class="login-title">MyBank</h2>
+    <form @submit.prevent="onSubmit" class="formContainer">
+      <h3>Iniciar sesión</h3>
+      <div class="inputContainer">
+        <label for="input-dni">Ingrese su identificador</label>
+        <b-form-input id="input-dni" type="number" placeholder="12345678" />
+      </div>
+      <b-button variant="outline-primary" value="Ingresar" type="submit"
+        >Ingresar</b-button
+      >
+    </form>
+  </div>
 </template>
 
 <script>
+import { saveLSItemData } from "../../utils/localStorageHelper";
 export default {
-
-}
+  name: "Login",
+  data: function() {
+    return {};
+  },
+  methods: {
+    onSubmit: function() {
+      const inputValue = document.getElementById("input-dni").value;
+      saveLSItemData("userId", inputValue);
+      this.$router.push({ path: "/home" });
+    },
+  },
+};
 </script>
 
-<style>
-
+<style escoped>
+.login-title {
+  color: white;
+  font-size: 50px;
+  width: 20rem;
+  text-align: initial;
+}
+.container-login {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgb(35, 139, 250);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.formContainer {
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 8px;
+  padding: 1rem;
+  min-height: 28rem;
+  min-width: 20rem;
+  justify-content: space-between;
+}
+.inputContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
 </style>
