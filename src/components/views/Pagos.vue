cd3bef58459d0e2f3d5033851f632da7c2f82768
ivanri <ivan.riboldi@outlook.com>
2021-06-01 17:00:05 -0300

    se agrega demo de vuex en pagos

--- src/components/views/Pagos.vue
+++ src/components/views/Pagos.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
     <h1>Pagos</h1>
-    <h1>VUEX {{ $store.state.counter }}</h1>
+    <h1>VUEX {{ $store.getters.getCounter }}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
   </div>
 </template>
-<script></script>
-<style></style>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  methods: {
+    increment: function() {
+      this.$store.dispatch("increment");
+    },
+    decrement: function() {
+      this.$store.dispatch("decrement");
+    },
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,25 @@ export default new Vuex.Store({
   state: {
     counter: 0,
   },
+  actions: {
+    increment(context) {
+      context.commit("increment");
+    },
+    decrement(context) {
+      context.commit("decrement");
+    },
+  },
   mutations: {
     increment(state) {
       state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+  },
+  getters: {
+    getCounter: (state) => {
+      return state.counter;
     },
   },
 });
