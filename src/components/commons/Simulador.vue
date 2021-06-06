36f9aad45d6427b376ee47567c86d887302243eb
Javi <javier.nuez91@gmail.com>
2021-06-06 00:35:06 -0300

    se le agrego funcionalidad a simulador

---
+++ src/components/commons/Simulador.vue
@@ -0,0 +1,64 @@
+<template>
+<div id="simulador" class="card text-white bg-info mb-2">  
+    <div class="card-header"><h1>Simulador</h1></div>
+    <div class="card-body">
+    <h4>Su préstamo:</h4>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">$</span>
+        </div>
+            <input id="n1" type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Ingrese monto..">
+    </div>
+		
+	<h4>Cuotas mensuales:</h4>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-default">$</span>
+        </div>
+            <input id="n2" type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Sus cuotas..">
+    </div>
+		
+		<h4>Interés anual: 37%</h4>
+		<h4>Capital con intereses:</h4> 
+        <h4>$<a id="salida"></a></h4>
+		<h4>Cuota promedio:</h4> 
+        <h4>$<a id="cuota"></a></h4>
+		
+		<button id="calcular" class="btn btn-light">Calcular</button>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  data: () => ({
+
+  }),
+  mounted() {
+    document.querySelector('#calcular').addEventListener('click', () =>{
+	const n1 = parseInt(document.querySelector('#n1').value);
+	const n2 = parseInt(document.querySelector('#n2').value);
+	
+	var cuotaPura = n1 / n2;
+	var interes = cuotaPura * 3.08*n2/100;
+	var cuotaConInteres = cuotaPura + interes;
+	var capital = cuotaConInteres * n2;
+	
+	document.querySelector('#salida').innerHTML = capital;
+	document.querySelector('#cuota').innerHTML = cuotaConInteres;
+    })
+}}
+</script>
+<style>
+#simulador{
+  
+  width: 450px;
+  margin-top: 45px;
+  margin-right: 5%;
+  margin-left: 5%;
+  color: rgba(255, 255, 255, 0.66);
+  border-radius: 5px;
+  background: #1e94a3;
+}
+</style>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,51 +1,41 @@
 <template>
-  <div>
-    <div class="row">
+  <div class="row">
+    
     <div v-if="cuentas.length === 0 && !usuarioSinCuentas">
       Cargando datos...
     </div>
     <div v-if="usuarioSinCuentas">Usted no tiene cuentas</div>
-    <div class="col">
-    <div v-for="(cuenta, index) in cuentas" :key="index">
-      <div class="row">
-        <div class="col">
-            <Grilla
-              :titulo="cuenta.moneda"
-              :data="cuenta.movimientos"
-              :columns="grillaTitulos"
-            ></Grilla>
+    <div>
+    <div class="row">
+        <div class="col-sm-8">
+          <div v-for="(cuenta, index) in cuentas" :key="index">
+            <div>
+              <Grilla
+                :titulo="cuenta.moneda"
+                :data="cuenta.movimientos"
+                :columns="grillaTitulos"
+              ></Grilla>
+            </div>
+          </div>
         </div>
+        <div class="col-sm-4">
+          <div>
+          <Simulador></Simulador>
+          </div>
         </div>
     </div>
+    </div>
+      
   </div>
-  <div id="simulador" class="col">
-    <h3>Simulador de prestamo</h3>
-    <form class="mid-form">
-      <div class="form-group">
-        <h4>Dinero</h4>
-        <input id="dinero" type="text" name= "dinero">
-      </div>
-      <div class="form-group">
-        <h4>Cuotas</h4>
-        <input id="cuotas" type="text" name= "cuotas">
-      </div>
-      <div class="form-group">
-        <h4>Interes TNA <a id="interes">37%</a></h4>
-        <h3><a id="resulado" >Mi prestamo</a></h3>
-      </div>
-
-      <input type="button" name="calcular" value="Simular" onclick="#" />
-    </form>
-  </div>
-  </div>
-  </div>
+  
+  
 </template>
 
 <script>
-
-
 import axios from "axios";
 import Grilla from "../commons/Grilla";
+import Simulador from "../commons/Simulador";
+
 
 export default {
   props: {},
@@ -75,20 +65,12 @@ export default {
   },
   components: {
     Grilla,
+    Simulador,
   },
 };
 
 </script>
 
 <style>
-#simulador{
-  float: center;
-  max-width: 550px;
-  margin-top: 45px;
-  margin-right: 5%;
-  margin-left: 5%;
-  color: rgba(255, 255, 255, 0.66);
-  border-radius: 5px;
-  background: #1e94a3;
-}
+
 </style>
