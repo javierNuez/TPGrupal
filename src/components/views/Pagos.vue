825b7519b6716a08706622a9f0d52022505876f0
Antonella Castellano <antonellacastellano97@gmail.com>
2021-06-06 20:54:58 -0300

    $ en formulario pago

--- src/components/views/Pagos.vue
+++ src/components/views/Pagos.vue
@@ -2,12 +2,12 @@
   <div>
     <h1>Pagos</h1>
     <div>
-    <Grilla
-      titulo="Detalle de los pagos"
-      :data="getGridData()"
-      :columns="getGridColumns()"
-    ></Grilla>
-    <br>
+      <Grilla
+        titulo="Detalle de los pagos"
+        :data="getGridData()"
+        :columns="getGridColumns()"
+      ></Grilla>
+      <br />
     </div>
     <div class="container">
       <div class="row">
@@ -20,8 +20,23 @@
             class="form"
           ></b-form-select>
           <label for="Monto">Monto</label>
-          <b-form-input type="number" v-model="texto"></b-form-input>
-          <b-button style="margin: 10px" variant="outline-primary" value="Pagar" type="submit"
+          <div>
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-default"
+                >$</span
+              >
+              <b-form-input
+                class="input-group"
+                type="number"
+                v-model="importe"
+              ></b-form-input>
+            </div>
+          </div>
+          <b-button
+            style="margin: 10px"
+            variant="outline-primary"
+            value="Pagar"
+            type="submit"
             >PAGAR</b-button
           >
         </div>
@@ -31,7 +46,7 @@
     <!-- <h1>VUEX {{ $store.getters.getCounter }}</h1>
     <button @click="increment">+</button>
     <button @click="decrement">-</button>    -->
-    {{ texto }}
+    {{ importe }}
   </div>
 </template>
 
@@ -41,7 +56,7 @@ export default {
   data() {
     return {
       selected: null,
-      texto: "",
+      importe: "",
       options: [
         { value: null, text: "Seleccionar el servio a pagar" },
         { value: "a", text: "Electricidad" },
@@ -49,14 +64,14 @@ export default {
         { value: "c", text: "ABL" },
         { value: "d", text: "Seguro Automotor" },
       ],
-      itemsData : [
-      { fecha: "10-10-2020", ServicioAbonado: "a", Importe: 0 },
-      { fecha: "20-6-2020", ServicioAbonado: "d", Importe: 6546 },
-      { fecha: "30-5-2020", ServicioAbonado: "c", Importe: 55 },
-      { fecha: "04-1-2020", ServicioAbonado: "t", Importe: 10 },
-      { fecha: "10-2-2020", ServicioAbonado: "k", Importe: 2 },
-    ],
-     gridColumns: ["fecha", "ServicioAbonado", "Importe"],
+      itemsData: [
+        { fecha: "10-10-2020", ServicioAbonado: "a", Importe: 0 },
+        { fecha: "20-6-2020", ServicioAbonado: "d", Importe: 6546 },
+        { fecha: "30-5-2020", ServicioAbonado: "c", Importe: 55 },
+        { fecha: "04-1-2020", ServicioAbonado: "t", Importe: 10 },
+        { fecha: "10-2-2020", ServicioAbonado: "k", Importe: 2 },
+      ],
+      gridColumns: ["fecha", "ServicioAbonado", "Importe"],
     };
   },
   beforeCreate() {
@@ -66,10 +81,10 @@ export default {
     Grilla,
   },
   methods: {
-    getGridData: function() {
+    getGridData: function () {
       return this.itemsData;
     },
-    getGridColumns: function() {
+    getGridColumns: function () {
       return this.gridColumns;
     },
   },
@@ -81,7 +96,6 @@ export default {
   //   //   this.$store.dispatch("decrement");
   //   //  },
   // },
-
 };
 </script>
 <style>
