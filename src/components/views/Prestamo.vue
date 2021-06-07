5a8a4bc10335b906df3d3c63b0da3ef0a5e9be40
Antonella Castellano <antonellacastellano97@gmail.com>
2021-06-07 20:14:08 -0300

    Intento de datos en prestamo

--- src/components/views/Prestamo.vue
+++ src/components/views/Prestamo.vue
@@ -2,42 +2,38 @@
   <div class="prestamoslayout">
     <Grilla
       titulo="Historial de prestamos"
-      :data="getGridData()"
-      :columns="getGridColumns()"
+      :data="cuentas.movimiento"
+      :columns="grillaTitulos"
     ></Grilla>
   </div>
 </template>
 
 <script>
 import Grilla from "../commons/Grilla";
+import axios from "axios";
+import { getLSItemData } from "../../utils/localStorageHelper";
 export default {
+
   data: function() {
-    const itemsData = [
-      { fecha: "10-10-2020", Movimiento: "a", Importe: 0 },
-      { fecha: "20-6-2020", Movimiento: "d", Importe: 6546 },
-      { fecha: "30-5-2020", Movimiento: "c", Importe: 55 },
-      { fecha: "04-1-2020", Movimiento: "t", Importe: 10 },
-      { fecha: "10-2-2020", Movimiento: "k", Importe: 2 },
-    ];
-    return {
-      gridData: itemsData,
-      gridColumns: ["fecha", "Movimiento", "Importe"],
+      return {
+      grillaTitulos: ["descripcion", "destino", "fecha", "monto", "origen"],
+      //cuentas: this.$store.getters.getCuentas,
+      usuarioSinCuentas: false,
     };
   },
+  methods: {},
   beforeCreate() {
-    this.gridData = [];
+    const dni = getLSItemData("userId");
+    axios
+      .get(`https://vuebank-api.herokuapp.com/accounts/${dni}`)
+      .then((res) => {
+        const datos = res.data;
+        if (!datos.length) return (this.usuarioSinCuentas = true);
+        this.$store.dispatch("setCuentas", datos);
+      });
   },
-
   components: {
     Grilla,
-  },
-  methods: {
-    getGridData: function() {
-      return this.gridData;
-    },
-    getGridColumns: function() {
-      return this.gridColumns;
-    },
   },
 };
 </script>
